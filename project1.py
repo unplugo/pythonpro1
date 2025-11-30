1fd6ce37f90aec83e3f29d6f36350f9c44724c35
Aman Srivastava <srivastavaaman88@gmail.com>
2025-11-30 10:41:51 +0530

    Initial upload

---
+++ project1.py
@@ -0,0 +1,12 @@
+num1 = int(input("enter a number one"))
+num2 = int(input("enter a number two"))
+
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+
+print("Addition:", addition)
+print("Subtraction:", subtraction)
+print("Multiplication:", multiplication)
+print("Division:", division)
